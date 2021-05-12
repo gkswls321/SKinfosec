e7b17a0576250696a1bcb1658fce165e577f5320
Beomkim95 <76420029+Beomkim95@users.noreply.github.com>
2021-05-12 17:16:34 +0900

    Update S3트리거-S3파일읽기-python38.py

--- 자유게시판/관련 람다코드 폴더/S3트리거-S3파일읽기-python38.py
+++ 자유게시판/관련 람다코드 폴더/S3트리거-S3파일읽기-python38.py
@@ -1,1 +1,33 @@
+import boto3
+import json
 
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+
+    if event:
+        print("Event : ", event)
+        # 파일의 이름 가져오기
+        file_obj = event["Records"][0]
+        filename = str(file_obj['s3']['object']['key'])
+        print('Filename: ', filename)
+        
+        # bucket 이름 찾기 
+        bucket_name = event['Records'][0]['s3']['bucket']['name']
+        print('bucket_name: ', bucket_name)
+
+        fileObj = s3.get_object(Bucket=bucket_name, Key=filename)
+        print("fileObj: ", fileObj)
+        # 가져올 파일
+        file_content = fileObj["Body"].read().decode('utf-8')
+        print('file_content: ', file_content)
+        
+        # 가져온 파일을 json 형식으로 변환
+        json_data = json.loads(file_content)
+        print('json : ',json_data)
+
+
+    return {
+            'age': json_data['age'] , 
+            'product': json_data['product'] , 
+            'customerID': json_data['customerID']
+        }
