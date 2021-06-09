8bfd1333f328401112d2768081c9cdde48c25a51
PeachLime <48670542+PeachLime@users.noreply.github.com>
2021-06-09 13:20:04 +0900

    Update cbs_aws_cli_lambdaS3_subprocess.py

--- 09-Source Code/합본코드/cbs_aws_cli_lambdaS3_subprocess.py
+++ 09-Source Code/합본코드/cbs_aws_cli_lambdaS3_subprocess.py
@@ -23,7 +23,15 @@ today_d = today.day
 # aws lambda invoke --function-name my-function out_log.txt --log-type Tail --query 'LogResult' --output text
 # 실행하는 람다 함수: my-function out
 
-awscli_lambdacall = "aws lambda invoke --function-name my-function out_log.txt --log-type Tail --query 'LogResult' --output text"
+# 웹로그를 저장하는 람다함수 실행하는 명령어
+awscli_lambdacall_web1_apache2 = "aws lambda invoke --function-name beom-cw-s3-web1-apache2 out_log.txt --log-type Tail --query 'LogResult' --output text"
+awscli_lambdacall_web1_mysql = "aws lambda invoke --function-name beom-cw-s3-web1-mysql out_log.txt --log-type Tail --query 'LogResult' --output text"
+awscli_lambdacall_web2_apache2 = "aws lambda invoke --function-name beom-cw-s3-web2-apache2 out_log.txt --log-type Tail --query 'LogResult' --output text"
+awscli_lambdacall_web2_mysql = "aws lambda invoke --function-name beom-cw-s3-web2-mysql out_log.txt --log-type Tail --query 'LogResult' --output text"
 
-subprocess.call(awscli_lambdacall, shell=True)
+# 명령어 실행
+subprocess.call(awscli_lambdacall_web1_apache2, shell=True)
+subprocess.call(awscli_lambdacall_web1_mysql, shell=True)
+subprocess.call(awscli_lambdacall_web2_apache2, shell=True)
+subprocess.call(awscli_lambdacall_web2_mysql, shell=True)
 
